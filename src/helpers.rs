6fbac10f4009f2882ca7fd0b387753e85d09b216
Florian Dieminger <me@fiji-flo.de>
2022-06-15 19:40:57 +0200

    (de)serializer tests + fix

--- src/helpers.rs
+++ src/helpers.rs
@@ -20,7 +20,7 @@ where
 
 pub mod serde_utc_seconds_f {
     use chrono::{DateTime, TimeZone, Utc};
-    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use serde::{Deserialize, Deserializer};
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
     where
@@ -36,11 +36,9 @@ pub mod serde_utc_seconds_f {
                     seconds
                 ))
             })?;
-            let secs = secs_f64.floor();
-            (
-                secs as i64,
-                ((secs_f64 - secs) * 1_000_000_000.).floor() as u32,
-            )
+            let secs = secs_f64.trunc();
+            let ms = (secs_f64.fract() * 1000f64).trunc() as u32;
+            (secs as i64, ms * 1_000_000)
         };
         Utc.timestamp_opt(secs, nsecs).single().ok_or_else(|| {
             serde::de::Error::custom(format!(
@@ -48,14 +46,6 @@ pub mod serde_utc_seconds_f {
                 seconds
             ))
         })
-    }
-
-    #[allow(dead_code)]
-    pub fn serialize<S>(v: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        v.timestamp().serialize(serializer)
     }
 }
 
@@ -88,6 +78,74 @@ pub mod serde_utc_milliseconds {
     where
         S: Serializer,
     {
-        (v.timestamp() * 1000).serialize(serializer)
+        (v.timestamp_millis()).serialize(serializer)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use anyhow::Error;
+    use chrono::{DateTime, NaiveDateTime, Utc};
+    use serde::Serialize;
+    use serde_json::json;
+
+    #[derive(Deserialize)]
+    struct StingOrVec {
+        #[serde(deserialize_with = "deserialize_string_or_vec")]
+        field: Vec<String>,
+    }
+
+    #[derive(Deserialize, Serialize)]
+    struct Millis {
+        #[serde(with = "serde_utc_milliseconds")]
+        dt: DateTime<Utc>,
+    }
+
+    #[derive(Deserialize)]
+    struct SecondsF {
+        #[serde(with = "serde_utc_seconds_f")]
+        dt: DateTime<Utc>,
+    }
+
+    #[test]
+    fn test_string_or_vec() -> Result<(), Error> {
+        let json = json!({ "field": "foo" });
+        let sov: StingOrVec = serde_json::from_value(json)?;
+        assert_eq!(&sov.field, &["foo".to_string()]);
+
+        let json = json!({ "field": ["foo", "bar"] });
+        let sov: StingOrVec = serde_json::from_value(json)?;
+        assert_eq!(&sov.field, &["foo".to_string(), "bar".to_string()]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_utc_milliseconds() -> Result<(), Error> {
+        let json = json!({ "dt": 1655312049699001i64 });
+        let dt_serde: Millis = serde_json::from_value(json.clone())?;
+        let dt =
+            DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1655312049699, 1_000_000), Utc);
+        assert_eq!(dt, dt_serde.dt);
+
+        let millis = Millis { dt };
+        let millis_json = serde_json::to_value(&millis)?;
+        assert_eq!(json, millis_json);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_utc_seconds_f() -> Result<(), Error> {
+        let json = json!({ "dt": 1655312049699.1f64 });
+        let dt_serde: SecondsF = serde_json::from_value(json)?;
+        let dt = DateTime::<Utc>::from_utc(
+            NaiveDateTime::from_timestamp(1655312049699, 100_000_000),
+            Utc,
+        );
+        assert_eq!(dt, dt_serde.dt);
+
+        Ok(())
     }
 }
