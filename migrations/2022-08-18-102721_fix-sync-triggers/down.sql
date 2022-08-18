96d1302f30740416568d9dc728d0344d0b263709
Guy Sleigh <gsleigh@mozilla.com>
2022-08-18 13:49:45 +0200

    reverse migration resets trigger stats to previous

--- migrations/2022-08-18-102721_fix-sync-triggers/down.sql
+++ migrations/2022-08-18-102721_fix-sync-triggers/down.sql
@@ -1,1 +1,49 @@
 -- This file should undo anything in `up.sql`
+-- This creates a collection_item and adds it to the user's default collection any time they create a V1 collection.
+CREATE OR REPLACE FUNCTION synchronize_collection_items()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    with USER_DEFAULT_COLLECTION as (select mcs.id      as collection_id,
+                                            NEW.user_id as user_id
+                                     from multiple_collections mcs
+                                     where user_id = NEW.user_id
+                                       and mcs.name = 'Default')
+    INSERT
+    INTO collection_items (created_at,
+                                      updated_at,
+                                      deleted_at,
+                                      document_id,
+                                      notes,
+                                      custom_name,
+                                      user_id, multiple_collection_id)
+    select NEW.created_at,
+           NEW.updated_at,
+           NEW.deleted_at,
+           NEW.document_id,
+           NEW.notes,
+           NEW.custom_name,
+           NEW.user_id,
+           mcs.id
+    from multiple_collections mcs
+    where user_id = NEW.user_id
+      and mcs.name = 'Default';
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger and function to update collection_items + default collections when old API is used.
+CREATE OR REPLACE FUNCTION update_collection_item()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE collection_items ci
+    set notes       = NEW.notes,
+        custom_name = NEW.custom_name,
+        deleted_at  = NEW.deleted_at,
+        updated_at  = NEW.updated_at
+    where ci.user_id = NEW.user_id
+      and ci.document_id = NEW.document_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
