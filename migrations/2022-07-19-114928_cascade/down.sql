de79a732dbea6368de542bf3899a51d601474707
Florian Dieminger <me@fiji-flo.de>
2023-01-31 16:31:10 +0100

    prepare for diesel 2.0.3 (#146)

--- migrations/2022-07-19-114928_cascade/down.sql
+++ migrations/2022-07-19-114928_cascade/down.sql
@@ -1,23 +1,8 @@
-BEGIN;
-ALTER TABLE collections DROP CONSTRAINT collections_user_id_fkey;
-ALTER TABLE collections ADD FOREIGN KEY (user_id)
-REFERENCES users(id) ON DELETE NO ACTION;
-COMMIT;
-
-BEGIN;
-ALTER TABLE settings DROP CONSTRAINT settings_user_id_fkey;
-ALTER TABLE settings ADD FOREIGN KEY (user_id)
-REFERENCES users(id) ON DELETE NO ACTION;
-COMMIT;
-
-BEGIN;
-ALTER TABLE watched_items DROP CONSTRAINT watched_items_user_id_fkey;
-ALTER TABLE watched_items ADD FOREIGN KEY (user_id)
-REFERENCES users(id) ON DELETE NO ACTION;
-COMMIT;
-
-BEGIN;
-ALTER TABLE notifications DROP CONSTRAINT notifications_user_id_fkey;
-ALTER TABLE notifications ADD FOREIGN KEY (user_id)
-REFERENCES users(id) ON DELETE NO ACTION;
-COMMIT;
+ALTER TABLE collections DROP CONSTRAINT collections_user_id_fkey,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION;
+ALTER TABLE settings DROP CONSTRAINT settings_user_id_fkey,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION;
+ALTER TABLE watched_items DROP CONSTRAINT watched_items_user_id_fkey,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION;
+ALTER TABLE notifications DROP CONSTRAINT notifications_user_id_fkey,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION;
--- migrations/2023-01-17-155729_ping-cascade/down.sql
+++ migrations/2023-01-17-155729_ping-cascade/down.sql
@@ -1,5 +1,2 @@
-BEGIN;
-ALTER TABLE activity_pings DROP CONSTRAINT activity_pings_user_id_fkey;
-ALTER TABLE activity_pings ADD FOREIGN KEY (user_id)
-REFERENCES users(id) ON DELETE NO ACTION;
-COMMIT;
+ALTER TABLE activity_pings DROP CONSTRAINT activity_pings_user_id_fkey,
+    ADD FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE NO ACTION;
