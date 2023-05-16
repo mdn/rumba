e8d3603591bbbf9854e0f5eafb18a4f1b9169140
Florian Dieminger <me@fiji-flo.de>
2023-05-16 09:57:05 +0200

    add test

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,4 @@
-use crate::helpers::app::test_app_with_login;
+use crate::helpers::app::{init_test, test_app_with_login};
 use crate::helpers::db::reset;
 use crate::helpers::http_client::TestHttpClient;
 use crate::helpers::{read_json, wait_for_stubr};
@@ -42,7 +42,10 @@ async fn settings_newsletter_test() -> Result<(), Error> {
 
     drop(stubr);
     let stubr = Stubr::start_blocking_with(
-        vec!["tests/stubs", "tests/test_specific_stubs/newsletter"],
+        vec![
+            "tests/stubs",
+            "tests/test_specific_stubs/newsletter/basket_lookup_user.json",
+        ],
         Config {
             port: Some(4321),
             latency: None,
@@ -83,6 +86,29 @@ async fn anonymous_newsletter_test() -> Result<(), Error> {
     let newsletter_res = test::call_service(&service, request).await;
 
     assert!(newsletter_res.status().is_success());
+
+    drop(stubr);
+    Ok(())
+}
+
+#[actix_rt::test]
+async fn anonymous_newsletter_error_test() -> Result<(), Error> {
+    let (_, stubr) = init_test(vec![
+        "tests/stubs",
+        "tests/test_specific_stubs/newsletter/basket_subscribe_error.json",
+    ])
+    .await?;
+
+    let pool = reset()?;
+    let app = test_app_with_login(&pool).await.unwrap();
+    let service = test::init_service(app).await;
+    let request = test::TestRequest::post()
+        .set_json(json!({ "email": "foo@bar.com"}))
+        .uri("/api/v1/newsletter")
+        .to_request();
+    let newsletter_res = test::call_service(&service, request).await;
+
+    assert!(newsletter_res.status().is_server_error());
 
     drop(stubr);
     Ok(())
