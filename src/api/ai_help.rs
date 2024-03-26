e851bacfc0f42c3a4ae999188687d9b46c83224d
Claas Augner <caugner@mozilla.com>
2024-03-26 17:36:01 +0100

    Revert "chore(ai-help): record response duration on error"
    This reverts commit 4090413bf321ccc240e7236d9f18739d63f2600e.

--- src/api/ai_help.rs
+++ src/api/ai_help.rs
@@ -402,7 +402,6 @@ pub async fn ai_help(
         }
         let user_id = user.id;
 
-        let start = Instant::now();
         match prepare_res {
             Ok(ai_help_req) => {
                 let sources = ai_help_req.refs;
@@ -417,6 +416,7 @@ pub async fn ai_help(
                     None => Utc::now(),
                 };
 
+                let start = Instant::now();
                 let ai_help_meta = AIHelpMeta {
                     typ: MetaType::Metadata,
                     chat_id,
@@ -522,7 +522,6 @@ pub async fn ai_help(
                 Ok(sse::Sse::from_stream(refs.chain(res_stream)))
             }
             Err(e) => {
-                let response_duration = start.elapsed();
                 let ai_help_message_meta = AiHelpMessageMetaInsert {
                     user_id: user.id,
                     chat_id,
@@ -531,7 +530,6 @@ pub async fn ai_help(
                     search_duration: default_meta_big_int(
                         ai_help_req_meta.search_duration.map(|d| d.as_millis()),
                     ),
-                    response_duration: default_meta_big_int(Some(response_duration.as_millis())),
                     query_len: default_meta_big_int(ai_help_req_meta.query_len),
                     context_len: default_meta_big_int(ai_help_req_meta.context_len),
                     model: ai_help_req_meta.model.unwrap_or(""),
