3026ea4edf3a110eb34501e56af3049be34b7df0
Florian Dieminger <me@fiji-flo.de>
2024-03-27 10:14:50 +0100

    use unwrap_or_default

--- src/ai/help.rs
+++ src/ai/help.rs
@@ -162,7 +162,7 @@ pub async fn prepare_ai_help_req(
         .model(config.model)
         .messages(messages)
         .temperature(0.0)
-        .stop(config.stop_phrase.unwrap_or(""))
+        .stop(config.stop_phrase.unwrap_or_default())
         .build()?;
     request_meta.model = Some(config.model);
 
